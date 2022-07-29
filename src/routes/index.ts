a74e5ea21afd0a10063c7e6a122271da492a196b
lucas_almeida <lucas.almdiniz@gmail.com>
2022-07-29 18:59:33 -0300

    feat: Requisito 4 - orderRoutes adicionado

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,9 @@
 import productRoutes from './products.routes';
 import userRoutes from './users.routes';
+import orderRoutes from './orders.routes';
 
 export default {
   productRoutes,
   userRoutes,
+  orderRoutes,
 };
