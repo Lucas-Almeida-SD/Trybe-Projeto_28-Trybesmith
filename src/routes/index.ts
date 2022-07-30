da6ff09a9a22ba0ba9ff4bd6907d9e5a20fe7fae
lucas_almeida <lucas.almdiniz@gmail.com>
2022-07-29 21:51:48 -0300

    feat: Requisito 5 - loginRoutes adicionado

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import productRoutes from './products.routes';
 import userRoutes from './users.routes';
 import orderRoutes from './orders.routes';
+import loginRoutes from './login.routes';
 
 export default {
   productRoutes,
   userRoutes,
   orderRoutes,
+  loginRoutes,
 };
