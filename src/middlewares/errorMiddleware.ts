09f3ba3f56d01ebd923119540ff546b6374d7eaa
lucas_almeida <lucas.almdiniz@gmail.com>
2022-07-30 19:45:51 -0300

    refactor: middlware refatorado para tratar a nova forma de lançamento de erros

--- src/middlewares/errorMiddleware.ts
+++ src/middlewares/errorMiddleware.ts
@@ -1,26 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
 import { StatusCodes } from 'http-status-codes';
-import EErrors from '../enumerates/errors.enumerate';
-
-const getErrorCodeAndMessage = (errorMessage: string) => {
-  const generateError = (code: number, message: string) => ({ code, message });
-
-  switch (errorMessage) {
-    case (EErrors.usernameRequired):
-      return generateError(StatusCodes.BAD_REQUEST, errorMessage);
-    case (EErrors.passwordRequired):
-      return generateError(StatusCodes.BAD_REQUEST, errorMessage);
-    case (EErrors.invalidUsernameOrPassword):
-      return generateError(StatusCodes.UNAUTHORIZED, errorMessage);
-    default:
-      return generateError(StatusCodes.INTERNAL_SERVER_ERROR, '');
-  }
-};
 
 export default (err: Error, _req: Request, res: Response, _next: NextFunction) => {
-  const { message: errorMessage } = err;
+  if (err.name === 'error object') {
+    const { code, message } = JSON.parse(err.message);
+    return res.status(code).json({ message });
+  }
 
-  const { code, message } = getErrorCodeAndMessage(errorMessage);
-
-  res.status(code).json({ message });
+  res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: err.message });
 };
