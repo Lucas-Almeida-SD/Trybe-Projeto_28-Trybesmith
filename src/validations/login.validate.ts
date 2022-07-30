3ebd56db490d62c57cd3617ae12521446803864e
lucas_almeida <lucas.almdiniz@gmail.com>
2022-07-30 19:47:57 -0300

    refactor: criada nova forma de lançar erro, no qual envia um objeto de erro na forma de string

--- src/validations/login.validate.ts
+++ src/validations/login.validate.ts
@@ -1,7 +1,10 @@
 import Joi from 'joi';
+import { StatusCodes } from 'http-status-codes';
 import ILogin from '../interfaces/login.interface';
+import generateError from '../helpers/generateError';
+import IUser from '../interfaces/users.interface';
 
-export default (userLogin: ILogin) => {
+export const checkCredentials = (userLogin: ILogin) => {
   const { username, password } = userLogin;
 
   const { error } = Joi.object({
@@ -9,5 +12,21 @@ export default (userLogin: ILogin) => {
     password: Joi.string().required(),
   }).validate({ username, password });
 
-  if (error) throw new Error(error.message);
+  if (!error) return;
+
+  const errorObject = generateError(StatusCodes.BAD_REQUEST, error.message);
+
+  const myError = new Error(JSON.stringify(errorObject));
+  myError.name = 'error object';
+
+  throw myError;
+};
+
+export const checkAuthorization = (user: IUser | undefined) => {
+  if (!user) {
+    const errorObject = generateError(StatusCodes.UNAUTHORIZED, 'Username or password invalid');
+    const myError = new Error(JSON.stringify(errorObject));
+    myError.name = 'error object';
+    throw myError;
+  }
 };
