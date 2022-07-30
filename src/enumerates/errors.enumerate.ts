929f658424ac59ba795bf2c7e726fc0a4130c6cf
lucas_almeida <lucas.almdiniz@gmail.com>
2022-07-30 19:50:37 -0300

    chore: arquivo deletado pois não será mais necessário

--- src/enumerates/errors.enumerate.ts
+++ src/enumerates/errors.enumerate.ts
@@ -2,6 +2,12 @@ enum EErrors {
   invalidUsernameOrPassword = 'Username or password invalid',
   usernameRequired = '"username" is required',
   passwordRequired = '"password" is required',
+  nameRequired = '"name" must be a string',
+  nameString = '"name" must be a string',
+  nameMin3Caracter = '"name" length must be at least 3 characters long',
+  amountRequired = '"amount" must be a string',
+  amountString = '"amount" must be a string',
+  amountMin3Caracter = '"amount" length must be at least 3 characters long',
 }
 
 export default EErrors;
