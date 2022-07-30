711ca7b46b21b8ca745690d815e7d6d33bcae254
lucas_almeida <lucas.almdiniz@gmail.com>
2022-07-30 20:25:57 -0300

    feat: Requisito 7 - validação adicionada na rota de criação de users

--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -1,6 +1,7 @@
 import UserModel from '../models/users.model';
 import IUser from '../interfaces/users.interface';
 import generateToken from '../helpers/generateToken';
+import usersValidate from '../validations/users.validate';
 
 export default class UserService {
   public model: UserModel;
@@ -10,6 +11,8 @@ export default class UserService {
   }
 
   public async create(user: IUser): Promise<string> {
+    usersValidate(user);
+    
     const userCreated = await this.model.create(user);
 
     const token = generateToken(userCreated);
