0339044723fb7cd4331f457b23734234ef6b0f8c
lucas_almeida <lucas.almdiniz@gmail.com>
2022-07-29 21:46:25 -0300

    feat: Requisito 5 - criado enumerate  de errors de login

---
+++ src/enumerates/errors.enumerate.ts
@@ -0,0 +1,7 @@
+enum EErrors {
+  invalidUsernameOrPassword = 'Username or password invalid',
+  usernameRequired = '"username" is required',
+  passwordRequired = '"password" is required',
+}
+
+export default EErrors;
