6a2cf9b21fb609166ef25736b69d1fdc501e2837
lucas_almeida <lucas.almdiniz@gmail.com>
2022-07-31 18:50:39 -0300

    test: criado arquivo de mocks para os testes

---
+++ tests/mocks/dataMocks.ts
@@ -0,0 +1,10 @@
+export const createProductRequest = {
+  name: 'Arco Escudo Invejável',
+  amount: '3 Gemas da Noite',
+}
+
+export const createProductResponse = {
+  id: 6,
+  name: "Arco Escudo Invejável",
+  amount: "3 Gemas da Noite"
+}
