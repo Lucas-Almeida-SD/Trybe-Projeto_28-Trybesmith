4661c1bb1265131b878b88ee65d4a0258cac2020
lucas_almeida <lucas.almdiniz@gmail.com>
2022-07-31 18:59:24 -0300

    test: criado mock getAllProductsResponse

--- tests/mocks/dataMocks.ts
+++ tests/mocks/dataMocks.ts
@@ -8,3 +8,36 @@ export const createProductResponse = {
   name: "Arco Escudo Invejável",
   amount: "3 Gemas da Noite"
 }
+
+export const getAllProductsResponse = [
+  {
+    id: 1,
+    name: 'Espada curta',
+    amount: "10 peças de ouro",
+    orderId: null,
+  },
+  {
+    id: 2,
+    name: 'Escudo desnecessariamente grande',
+    amount: '20 peças de ouro',
+    orderId: 1,
+  },
+  {
+    id: 3,
+    name: 'Adaga de Aço Valírico',
+    amount: '1 peça de ouro',
+    orderId: 2,
+  },
+  {
+    id: 4,
+    name: 'Colar de fogo',
+    amount: '1 peça de ouro',
+    orderId: 2,
+  },
+  {
+    id: 5,
+    name: 'Engenhoca aleatória',
+    amount: '15 peças de ouro',
+    orderId: 3,
+  },
+]
