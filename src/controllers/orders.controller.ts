37b1620e61f7c558f4cf02c9821133396b5fe9f1
lucas_almeida <lucas.almdiniz@gmail.com>
2022-07-31 13:22:01 -0300

    feat: Requisito 8 - criada camada controller para rota "POST /orders"

--- src/controllers/orders.controller.ts
+++ src/controllers/orders.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import IToken from '../interfaces/token.interface';
 import OrderService from '../services/orders.service';
+import tokenValidate from '../validations/token.validate';
 
 export default class OrderController {
   public service: OrderService;
@@ -13,5 +15,16 @@ export default class OrderController {
     const orders = await this.service.getAll();
 
     res.status(StatusCodes.OK).json(orders);
+  };
+
+  public create = async (req: Request, res: Response) => {
+    const token = req.headers.authorization as string;
+    const { productsIds } = req.body;
+
+    const decoded = tokenValidate(token) as IToken;
+
+    await this.service.create(decoded.id, productsIds);
+
+    res.status(StatusCodes.CREATED).json({ userId: decoded.id, productsIds });
   };
 }
