aca63af03c92ad8c7faee1145c50a2940e9e3d6f
lucas_almeida <lucas.almdiniz@gmail.com>
2022-07-31 13:15:21 -0300

    feat: Requisito 8 - criada interface IProductUpdate e adicionada propriedade "orderId" à interface IProduct

--- src/interfaces/products.inteface.ts
+++ src/interfaces/products.inteface.ts
@@ -2,4 +2,11 @@ export default interface IProduct {
   id?: number,
   name: string,
   amount: string,
+  orderId: number,
+}
+
+export interface IProductUpdate {
+  name?: string,
+  amount?: string,
+  orderId?: number,
 }
