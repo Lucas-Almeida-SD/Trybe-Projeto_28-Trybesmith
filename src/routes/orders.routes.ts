6425911d15ce4f57e05cf3fd1774108805874f15
lucas_almeida <lucas.almdiniz@gmail.com>
2022-07-31 13:29:39 -0300

    feat: Requisito 8 - criada rota "POST /orders"

--- src/routes/orders.routes.ts
+++ src/routes/orders.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import OrderController from '../controllers/orders.controller';
+import authorization from '../middlewares/authorization';
+import 'express-async-errors';
 
 const orderRoutes = Router();
 
 const orderController = new OrderController();
 
 orderRoutes.get('/', orderController.getAll);
+orderRoutes.post('/', authorization, orderController.create);
 
 export default orderRoutes;
