ec10ac88bde65f8e7ef2118043f045d84558aa2b
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-13 00:10:22 -0300

    feat: objeto "user" que contém os dados do usuário a partir do token foi adicionado ao objeto da request

--- src/middlewares/authorization.ts
+++ src/middlewares/authorization.ts
@@ -3,8 +3,10 @@ import tokenValidate from '../validations/token.validate';
 
 export default (req: Request, res: Response, next: NextFunction) => {
   const token = req.headers.authorization as string;
+  
+  const user = tokenValidate(token);
 
-  tokenValidate(token);
+  req.user = user;
 
   next();
 };
